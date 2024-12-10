537266ad2b559fcb589d29f3f9447ea2a7aab32f
Tavi <octaviancilibia12@gmail.com>
2024-12-10 06:34:35 +0200

    First front end link

---
+++ run.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    pdf = False  # By default, no PDF is shown
+
+    if request.method == 'POST':
+        if request.form.get('action') == 'load_pdf':
+            pdf = True  # Set the variable to display the PDF
+
+    return render_template("index.html", pdf=pdf)
+
+# Main driver
+if __name__ == '__main__':
+    app.run(debug=True)
